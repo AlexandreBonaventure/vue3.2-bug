4778eb90ab1ff4211107bcf9eaf9cee336cbf3b3
Alexandre Geissmann <alex@la-bonaventure.com>
2021-07-20 11:20:56 -0400

    update for 3.2.0-beta.2

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,32 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-
+import { h, defineComponent } from 'vue';
+import { createRouter, createWebHistory, RouteRecordRaw, RouterView } from 'vue-router';
+import PassThrough from './PassThrough.vue'
 const routes: RouteRecordRaw[] = [
   {
-    name: 'a',
-    path: '/a/:id',
-    redirect: { name: 'child-a' },
-    props: true,
-    component: () => import('./ARoute.vue'),
+    path: '/',
+    component: PassThrough,
     children: [
       {
-        name: 'child-a',
-        path: 'child-a',
-        props: route => ({ id: route.params.id }),
-        component: {},
+        name: 'a',
+        path: 'a/:id',
+        redirect: { name: 'child-a' },
+        props: true,
+        component: () => import('./ARoute.vue'),
+        children: [
+          {
+            name: 'child-a',
+            path: 'child-a',
+            props: route => ({ id: route.params.id }),
+            component: { render: () => h('div', 'A content') },
+          },
+        ],
       },
-    ],
-  },
-  {
-    name: 'b',
-    path: '/b',
-    component: {},
+      {
+        name: 'b',
+        path: 'b',
+        component: { render: () => h('div', 'B content') },
+      },
+    ]
   },
 ];
 
